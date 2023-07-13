67eee093736b56530f2678497e8cd7e55d457518
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-13 09:49:56 -0600

    Figure out name initialize

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+class Player
+  attr_accessor :name, :monthly_cost, :contract_length, :total_cost
+
+  def initialize(name, monthly_cost, contract_length)
+    @name = name
+    @first_name = first_name
+    @last_name = last_name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @total_cost = total_cost
+  end
+
+
+
+
+end
