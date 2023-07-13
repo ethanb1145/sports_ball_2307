9c518d69546de8edf9b6317b863a045cf48eb42e
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-13 10:10:26 -0600

    Finish Iteration-1

--- lib/player.rb
+++ lib/player.rb
@@ -6,11 +6,8 @@ class Player
     @first_name, @last_name = name.split(' ')
     @monthly_cost = monthly_cost
     @contract_length = contract_length
-    @total_cost = total_cost
+    @total_cost = (monthly_cost)*(contract_length)
   end
-
-
-
 
 
 
