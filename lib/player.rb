eb32756982d43208d9bdb3e837de5f12666a2310
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-13 10:06:36 -0600

    Thank Google, use initialize to split name

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,15 @@
 class Player
-  attr_accessor :name, :monthly_cost, :contract_length, :total_cost
+  attr_accessor :name, :first_name, :last_name, :monthly_cost, :contract_length, :total_cost
 
   def initialize(name, monthly_cost, contract_length)
     @name = name
-    @first_name = first_name
-    @last_name = last_name
+    @first_name, @last_name = name.split(' ')
     @monthly_cost = monthly_cost
     @contract_length = contract_length
     @total_cost = total_cost
   end
+
+
 
 
 
