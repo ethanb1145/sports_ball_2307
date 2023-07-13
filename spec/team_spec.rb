0ae70e97d854c2175815e44699f9a83f3770755a
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-13 10:50:37 -0600

    Remove duplicate test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -9,14 +9,7 @@ RSpec.describe Player do
     expect(team).to be_a Team
   end
 
-  it "can show us the roster of players" do 
-    team = Team.new("Dodgers", "Los Angeles")
-
-    expect(team.roster).to eq([])
-
-  end
-
-  it "can add people to the roster" do 
+  it "can add people to the roster and show people on the roster" do 
     team = Team.new("Dodgers", "Los Angeles")
     player_1 = Player.new("Michael Palledorous", 1000000, 36)
     team.add_player(player_1)
